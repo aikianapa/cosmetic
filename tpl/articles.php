a6207f61a0e2f3a45a500ca6956614a0c3762cb8
Oleg Frolov <oleg_frolov@mail.ru>
2020-08-04 14:01:08 +0300

    update

---
+++ tpl/articles.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html>
+<wb-include wb-tpl="wrapper.inc.php" />
+
+<wb-jq wb-html="main">
+   
+    <!--breadcrumbs area start-->
+    <div class="breadcrumbs_area">
+        <div class="container">   
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcrumb_content">
+                        <ul>
+                            <li><a href="index.html">home</a></li>
+                            <li>blog</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>         
+    </div>
+    <!--breadcrumbs area end-->
+    
+    <!--blog area start-->
+    <div class="blog_page_section mb-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9 col-md-12">
+                    <div class="blog_wrapper">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-6">
+                                <article class="single_blog">
+                                    <figure>
+                                        <div class="blog_thumb">
+                                            <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/blog1.jpg" alt=""></a>
+                                        </div>
+                                        <figcaption class="blog_content">
+                                           <h4 class="post_title"><a href="blog-details.html"> Lorem ipsum dolor sit amet elit quia amet iusto</a></h4>
+                                           <p>Posted by Demo Hasthemes <span>Mar 25, 2020</span></p>
+                                           <footer class="blog_footer">
+                                                <a href="blog-details.html">+ Read More</a>
+                                            </footer>
+                                        </figcaption>
+                                    </figure>
+                                </article>
+                            </div>
+                            <div class="col-lg-4 col-md-6">
+                                <article class="single_blog">
+                                    <figure>
+                                        <div class="blog_thumb">
+                                            <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/blog2.jpg" alt=""></a>
+                                        </div>
+                                        <figcaption class="blog_content">
+                                           <h4 class="post_title"><a href="blog-details.html">Corrupti velit id sunt, porro minus, aliquid iure odio.</a></h4>
+                                           <p>Posted by Demo Hasthemes <span>Mar 25, 2020</span></p>
+                                           <footer class="blog_footer">
+                                                <a href="blog-details.html">+ Read More</a>
+                                            </footer>
+                                        </figcaption>
+                                    </figure>
+                                </article>
+                            </div>
+                            <div class="col-lg-4 col-md-6">
+                                <article class="single_blog">
+                                    <figure>
+                                        <div class="blog_thumb">
+                                            <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/blog3.jpg" alt=""></a>
+                                        </div>
+                                        <figcaption class="blog_content">
+                                           <h4 class="post_title"><a href="blog-details.html">Voluptas nemo adipisci ipsam dolore nisi facilis is.</a></h4>
+                                           <p>Posted by Demo Hasthemes <span>Mar 25, 2020</span></p>
+                                           <footer class="blog_footer">
+                                                <a href="blog-details.html">+ Read More</a>
+                                            </footer>
+                                        </figcaption>
+                                    </figure>
+                                </article>
+                            </div>
+                            <div class="col-lg-4 col-md-6">
+                                <article class="single_blog">
+                                    <figure>
+                                        <div class="blog_thumb">
+                                            <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/blog4.jpg" alt=""></a>
+                                        </div>
+                                        <figcaption class="blog_content">
+                                           <h4 class="post_title"><a href="blog-details.html">Iusto dolor cumque sequi dicta quis,  vel libero ullam.</a></h4>
+                                           <p>Posted by Demo Hasthemes <span>Mar 25, 2020</span></p>
+                                           <footer class="blog_footer">
+                                                <a href="blog-details.html">+ Read More</a>
+                                            </footer>
+                                        </figcaption>
+                                    </figure>
+                                </article>
+                            </div>
+                            <div class="col-lg-4 col-md-6">
+                                <article class="single_blog">
+                                    <figure>
+                                        <div class="blog_thumb">
+                                            <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/blog1.jpg" alt=""></a>
+                                        </div>
+                                        <figcaption class="blog_content">
+                                           <h4 class="post_title"><a href="blog-details.html">Eaque ipsa id pariatur, itaque maxime quasi ipsum</a></h4>
+                                           <p>Posted by Demo Hasthemes <span>Mar 25, 2020</span></p>
+                                           <footer class="blog_footer">
+                                                <a href="blog-details.html">+ Read More</a>
+                                            </footer>
+                                        </figcaption>
+                                    </figure>
+                                </article>
+                            </div>
+                            <div class="col-lg-4 col-md-6">
+                                <article class="single_blog">
+                                    <figure>
+                                        <div class="blog_thumb">
+                                            <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/blog2.jpg" alt=""></a>
+                                        </div>
+                                        <figcaption class="blog_content">
+                                           <h4 class="post_title"><a href="blog-details.html">Odit autem quidem alias fugiat in. Deleniti unde</a></h4>
+                                           <p>Posted by Demo Hasthemes <span>Mar 25, 2020</span></p>
+                                           <footer class="blog_footer">
+                                                <a href="blog-details.html">+ Read More</a>
+                                            </footer>
+                                        </figcaption>
+                                    </figure>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                    <!--blog pagination area start-->
+                    <div class="blog_pagination">
+                        <div class="pagination">
+                            <ul>
+                                <li class="current">1</li>
+                                <li><a href="blog.html#">2</a></li>
+                                <li><a href="blog.html#">3</a></li>
+                                <li class="next"><a href="blog.html#">next</a></li>
+                                <li><a href="blog.html#">>></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!--blog pagination area end-->
+                </div>  
+                <div class="col-lg-3 col-md-12">
+                    <div class="blog_sidebar_widget">
+                        <div class="widget_list widget_search">
+                            <div class="widget_title">
+                                <h3>Search</h3>
+                            </div>
+                            <form action="blog.html#">
+                                <input placeholder="Search..." type="text">
+                                <button type="submit">search</button>
+                            </form>
+                        </div>
+                        <div class="widget_list comments">
+                           <div class="widget_title">
+                                <h3>Recent Comments</h3>
+                            </div>
+                            <div class="post_wrapper">
+                                <div class="post_thumb">
+                                    <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/comment2.png.jpg" alt=""></a>
+                                </div>
+                                <div class="post_info">
+                                    <span> <a href="blog.html#">demo</a> says:</span>
+                                    <a href="blog-details.html">Quisque semper nunc</a>
+                                </div>
+                            </div>
+                             <div class="post_wrapper">
+                                <div class="post_thumb">
+                                    <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/comment2.png.jpg" alt=""></a>
+                                </div>
+                                <div class="post_info">
+                                    <span><a href="blog.html#">admin</a> says:</span>
+                                    <a href="blog-details.html">Quisque orci porta...</a>
+                                </div>
+                            </div>
+                            <div class="post_wrapper">
+                                <div class="post_thumb">
+                                    <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/comment2.png.jpg" alt=""></a>
+                                </div>
+                                <div class="post_info">
+                                    <span><a href="blog.html#">demo</a> says:</span>
+                                    <a href="blog-details.html">Quisque semper nunc</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="widget_list widget_post">
+                            <div class="widget_title">
+                                <h3>Recent Posts</h3>
+                            </div>
+                            <div class="post_wrapper">
+                                <div class="post_thumb">
+                                    <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/blogs1.jpg" alt=""></a>
+                                </div>
+                                <div class="post_info">
+                                    <h4><a href="blog-details.html">Blog image post</a></h4>
+                                    <span>March 16, 2018 </span>
+                                </div>
+                            </div>
+                             <div class="post_wrapper">
+                                <div class="post_thumb">
+                                    <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/blogs2.jpg" alt=""></a>
+                                </div>
+                                <div class="post_info">
+                                    <h4><a href="blog-details.html">Post with Gallery</a></h4>
+                                    <span>March 16, 2018 </span>
+                                </div>
+                            </div>
+                             <div class="post_wrapper">
+                                <div class="post_thumb">
+                                    <a href="blog-details.html"><img data-src="{{_var.base}}/assets/img/blog/blogs3.jpg" alt=""></a>
+                                </div>
+                                <div class="post_info">
+                                    <h4><a href="blog-details.html">Post with Audio</a></h4>
+                                    <span>March 16, 2018 </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="widget_list widget_categories">
+                            <div class="widget_title">
+                                <h3>Categories</h3>
+                            </div>
+                            <ul>
+                                <li><a href="blog.html#">Audio</a></li>
+                                <li><a href="blog.html#">Company</a></li>
+                                <li><a href="blog.html#">Gallery</a></li>
+                                <li><a href="blog.html#">Image</a></li>
+                            </ul>
+                        </div>
+                        <div class="widget_list widget_tag">
+                            <div class="widget_title">
+                                <h3>Tag products</h3>
+                            </div>
+                            <div class="tag_widget">
+                                <ul>
+                                    <li><a href="blog.html#">asian</a></li>
+                                    <li><a href="blog.html#">brown</a></li>
+                                    <li><a href="blog.html#">euro</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--blog area end-->
+   
+   <!--brand area start-->
+    <div class="brand_area brand_padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="brand_container owl-carousel ">
+                        <div class="single_brand">
+                            <a href="blog.html#"><img data-src="{{_var.base}}/assets/img/brand/brand1.jpg" alt=""></a>
+                        </div>
+                        <div class="single_brand">
+                            <a href="blog.html#"><img data-src="{{_var.base}}/assets/img/brand/brand2.jpg" alt=""></a>
+                        </div>
+                        <div class="single_brand">
+                            <a href="blog.html#"><img data-src="{{_var.base}}/assets/img/brand/brand3.jpg" alt=""></a>
+                        </div>
+                        <div class="single_brand">
+                            <a href="blog.html#"><img data-src="{{_var.base}}/assets/img/brand/brand4.jpg" alt=""></a>
+                        </div>
+                        <div class="single_brand">
+                            <a href="blog.html#"><img data-src="{{_var.base}}/assets/img/brand/brand5.jpg" alt=""></a>
+                        </div>
+                        <div class="single_brand">
+                            <a href="blog.html#"><img data-src="{{_var.base}}/assets/img/brand/brand1.jpg" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--brand area end-->
+
+</wb-jq>
